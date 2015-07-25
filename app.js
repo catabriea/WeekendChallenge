f4b80be7f3d440be8787c09813ac784c1d1dd59f
catabriea <white.catabriea@gmail.com>
2015-07-25 15:18:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -48,28 +48,30 @@ $(document).ready(function(){
     console.log(employeeArray);
     });
     function randomEmployee (min, max) {
-        return Math.floor(Math.random() * (1 + max - min) + min);
+        return Math.floor((Math.random() * (1 + max - min) + min));
     }
 
     $("#newEmployeeButton").on("click", function(event){
         event.preventDefault();
 
-        var employeeList={};
-    function Employee(){
-            this.firstName = firstName[randomemployee(0,17)];;
-            this.lastName = lastName[randomemployee(0,10)];;
-            this.num = randomemployee(1, 1000);
-            this.employeeTitle = newEmployeeTitle[randomemployee(0,7)];;
-            this.newEmployeeReviewScore = randomemployee(1,5);;
-            this.salary = randomemployee(35000, 550000);;
+        var employeeList=[];
+
+    function Employee(min, max){
+            this.firstName = firstName[randomEmployee(0,18)];;
+            this.lastName = lastName[randomEmployee(0,10)];;
+            this.num = randomEmployee(0,3500);
+            this.employeetitle = newemployeeTitle[randomEmployee(0,7)];
+            this.reviewNum = randomEmployee(1,5);;
+            this.salary = randomEmployee(35000, 550000);;
     };
 
         var firstName =["Leonardo", "Bryan", "William", "Orion", "Michael", "Flora", "Aurora", "Angelita", "Belle", "Verona", "Catabriea", "Erin", "Fabrisha", "Sandriea", "Lisa", "Matthew", "Smaug", "Frodo"];
         var lastName =["White", "Muellerleile", "Fontanilla", "Kruse", "Parvin", "Villanueva", "Petty", "Sandoval", "Cefalu", "Mortela"];
-        var newEmployeeTitle =["Baker", "Cashier", "Delivery", "Maintenance", "Operator", "Manager", "Decorator"];           
+        var newemployeeTitle =["Baker", "Cashier", "Delivery", "Maintenance", "Operator", "Manager", "Decorator"];           
         var newRandomEmployee = new Employee();
+        var reviewNum =(1, 2, 3, 4, 5);
         employeeArray.push(newRandomEmployee);
-        randomAddToList(newRandomEmployee.firstName, newRandomEmployee.lastName, newRandomEmployee.employeetitle, newRandomEmployee.num, newRandomEmployee.newemployeereviewscore, newRandomEmployee.salary);
+        randomAddToList(newRandomEmployee.firstName, newRandomEmployee.lastName, newRandomEmployee.employeetitle, newRandomEmployee.num, newRandomEmployee.reviewNum, newRandomEmployee.salary);
 
   
     function randomAddToList(first, last, num, title, review, salary){
@@ -81,7 +83,6 @@ $(document).ready(function(){
 
     console.log(employeeArray);
     });
+
 });
-
-//why wont random employee fire? I defined it...frustrating!
-
+//finallyfigured out wy my random employee wouldn't fire - cassie pointed out that my randomEmployee was randomemploee on some - fixed it and it worked...the undefineds were coming up from line 74 when i didn't call reviewNum and num properly
